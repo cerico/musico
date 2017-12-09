6438d147fcf76606d34250facb089b459b3d9f9d
cerico <garethrobertlee@gmail.com>
2017-12-09 09:37:58 +0000

    play

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,3 @@
-"rsync --rsh=ssh -av server.js root@188.226.163.24:/var/www/html/musico"
-"rsync --rsh=ssh -av dist/* root@188.226.163.24:/var/www/html/musico/dist"
+app=$(grep name package.json | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
+rsync --rsh=ssh -av server.js root@188.226.163.24:/var/www/html/musico
+rsync --rsh=ssh -av dist root@188.226.163.24:/var/www/html/musico
